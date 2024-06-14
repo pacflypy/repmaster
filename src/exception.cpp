af66c97bf19a30c6d88345a450b33f57121f686d
pacflypy <pacflypy@outlook.com>
2024-06-14 20:05:52 +0200

    Update

--- src/exception.cpp
+++ src/exception.cpp
@@ -1,1 +1,9 @@
 #include "exception.hpp"
+
+ControlFileError::ControlFileError(const std::string& message) : std::runtime_error("Control File Error: " + message) {}
+
+ReleaseFileError::ReleaseFileError(const std::string& message) : std::runtime_error("Release File Error: " + message) {}
+
+DebianPackageInvalidError::DebianPackageInvalidError(const std::string& message) : std::runtime_error("Debian Package Invalid: " + message) {}
+
+PathNotFoundError::PathNotFoundError(const std::string& message) : std::runtime_error("Path Not Found: " + message) {}
