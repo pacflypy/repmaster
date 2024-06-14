01acacd48873ec11c6e48379f4ad76887944162d
pacflypy <pacflypy@outlook.com>
2024-06-14 15:58:51 +0200

    update

---
+++ src/path.cpp
@@ -0,0 +1,17 @@
+#include "path.hpp"
+#include <cstdlib>
+#include <string>
+#include <stdexcept>  // Für std::runtime_error
+
+namespace RepoManager
+{
+    std::string GetHome()
+    {
+        const char* home_cstr = getenv("HOME");
+        if (home_cstr) {
+            return std::string(home_cstr);
+        } else {
+            throw std::runtime_error("HOME environment variable is not set");
+        }
+    }
+}
---
+++ src/path.hpp
@@ -0,0 +1,18 @@
+#ifndef PATH_HPP
+#define PATH_HPP
+
+#include <string>
+
+namespace RepoManager
+{
+    std::string GetHome();
+    std::string GetPrefix();
+    std::string GetConfig(std::string repo_name);
+    std::string GetUser();
+    std::string GetProject(std::string repo_name);
+    std::string GetSample(std::string prefix);
+    std::string GetDist(std::string project, std::string distribution, std::string component, std::string architecture);
+    std::string GetPool(std::string project, std::string deb_name);
+}
+
+#endif
